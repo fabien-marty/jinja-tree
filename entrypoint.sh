61c0fdca3cb71b86ef43ae00ac3830ea30612bb9
Fabien MARTY <fabien.marty@botify.com>
2025-03-31 17:31:42 +0200

    add smooth transition for new docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-cd /code || exit 1
+if ! test -d /code; then
+  # DEPRECATED: only for smooth transition
+  echo "WARNING: usage of /workdir is DEPRECATED => replace /workdir by /code"
+  cd /workdir || exit 1
+else
+  cd /code
+fi
 exec /app/.tmp/bin/uv run /app/.venv/bin/jinja-tree "$@"
 
 
